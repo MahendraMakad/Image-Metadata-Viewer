c247008b329407d126858e4d35444f515a1f5377
MahendraMakad <87107054+MahendraMakad@users.noreply.github.com>
2023-04-27 16:02:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,30 @@ function previewImage(event) {
     let gpsLatitude = EXIF.getTag(this, "GPSLatitude");
     let gpsLongitude = EXIF.getTag(this, "GPSLongitude");
     let gpsAltitude = EXIF.getTag(this, "GPSAltitude");
-
+    let exifVersion = EXIF.getTag(this,"ExifVersion");
+    let colorspace = EXIF.getTag(this,"ColorSpace");
+    let compressedBitsPerPixel = EXIF.getTag(this,"CompressedBitsPerPixel");
+    let exposureTime = EXIF.getTag(this,"ExposureTime");// in seconds
+    let flash = EXIF.getTag(this,"Flash");
+    let shutterSpeed = EXIF.getTag(this,"ShutterSpeedValue");
+    let brightnessValue = EXIF.getTag(this,"BrightnessValue");
+    let copyright = EXIF.getTag(this,"Copyright");
+    let imageDescription = EXIF.getTag(this,"ImageDescription");
+    let orientation = EXIF.getTag(this,"Orientation");
+    let sharpness = EXIF.getTag(this,"Sharpness");
+    let saturation = EXIF.getTag(this,"Saturation");
+    let contrast = EXIF.getTag(this,"Contrast");
+    let subjectDistanceRange = EXIF.getTag(this,"SubjectDistanceRange");
+    let whiteBalance = EXIF.getTag(this,"WhiteBalance");
+    let gainControl = EXIF.getTag(this,"GainControl");
+    let sensingMethod = EXIF.getTag(this,"SensingMethod");
+    let lightSource = EXIF.getTag(this,"LightSource");
+    let resolutionUnit = EXIF.getTag(this,"FocalPlaneResolutionUnit")
+    let XResolution = EXIF.getTag(this,"FocalPlaneXResolution");
+    let YResolution = EXIF.getTag(this,"FocalPlaneYResolution");
+    let fNumber = EXIF.getTag(this,"FNumber");
+    EXIF.getTag(this,);
+    EXIF.getTag(this,);
     // Display metadata
     metadataList.innerHTML = "";
 
@@ -76,6 +99,28 @@ function previewImage(event) {
     addMetaDataItem({ title: "GPS latitude: ", value: gpsLatitude });
     addMetaDataItem({ title: "GPS longitude: ", value: gpsLongitude });
     addMetaDataItem({ title: "GPS altitude: ", value: gpsAltitude });
+    addMetaDataItem({ title: "EXIF Version: ", value: exifVersion });
+    addMetaDataItem({ title: "Color Space: ", value: colorspace });
+    addMetaDataItem({ title: "Compressed Bits/Pixel: ", value: compressedBitsPerPixel });
+    addMetaDataItem({ title: "Exposure Time(sec): ", value: exposureTime });
+    addMetaDataItem({ title: "Flash: ", value: flash });
+    addMetaDataItem({ title: "Contrast: ", value: contrast });
+    addMetaDataItem({ title: "Brightness: ", value: brightnessValue });
+    addMetaDataItem({ title: "Shutter Speed: ", value: shutterSpeed });
+    addMetaDataItem({ title: "Gain Control: ", value: gainControl });
+    addMetaDataItem({ title: "Sharpness: ", value: sharpness });
+    addMetaDataItem({ title: "Orientation: ", value: orientation });
+    addMetaDataItem({ title: "Saturation: ", value: saturation });
+    addMetaDataItem({ title: "Sensing Method: ", value: sensingMethod });
+    addMetaDataItem({ title: "Light Source: ", value: lightSource });
+    addMetaDataItem({ title: "White Balance: ", value: whiteBalance });
+    addMetaDataItem({ title: "Subject Distance Range: ", value: subjectDistanceRange });
+    addMetaDataItem({ title: "Image Description: ", value: imageDescription });
+    addMetaDataItem({ title: "Copyright: ", value: copyright });
+    addMetaDataItem({ title: "Resolution Unit: ", value: resolutionUnit });
+    addMetaDataItem({ title: "X-Resolution: ", value: XResolution });
+    addMetaDataItem({ title: "Y-Resolution: ", value: YResolution });
+    addMetaDataItem({ title: "F-Number: ", value: fNumber });
     fileDataContainer.style.display = "block";
     metaDataContainer.style.display = "block";
   });
